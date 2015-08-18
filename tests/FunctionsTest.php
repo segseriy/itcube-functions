a78ec1d84b5c4399e670da555295d4a84c589df1
Sergey <sergseriy@gmail.com>
2015-08-18 21:04:37 +0200

    tests

---
+++ tests/FunctionsTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Sergey
+ * Date: 18.08.2015
+ * Time: 14:41
+ */
+class FunctionsTest extends PHPUnit_Framework_TestCase
+{
+    protected $fixture;
+
+    /**
+     * @dataProvider providerArrayCount
+     */
+    public function testArrayCountSimple($a, $b, $c){
+        $curval = $this->fixture->array_count($a);
+        $this->assertEquals($b, $curval);
+    }
+
+    /**
+     * @dataProvider providerArrayCount
+     */
+    public function testArrayCountRecursive($a, $b, $c){
+        $curval = $this->fixture->array_count($a, 1);
+        $this->assertEquals($c, $curval);
+    }
+
+    public function providerArrayCount(){
+        return array(
+            array(
+                array( 1, 2, 3 ), 3, 3
+            ),
+            array(
+                array( 1, 2, array( 3 ) ), 3, 4
+            ),
+            array(
+                array( 1, 2, array( 3, array( 4, 5 ) ) ), 3, 7
+            )
+        );
+    }
+
+    protected function setUp()
+    {
+        $this->fixture = new \itcube\Functions();
+    }
+
+    protected function tearDown()
+    {
+        $this->fixture = NULL;
+    }
+}
